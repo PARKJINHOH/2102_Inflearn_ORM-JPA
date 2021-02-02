9ea1a1c9b2811df458b6a2d6ba691712ff4a36ea
Jinho <adg0609@ubivelox.com>
2021-02-02 09:15:03 +0900

    라인 추가

--- src/main/java/com/spring/jpa/JpaApplication.java
+++ src/main/java/com/spring/jpa/JpaApplication.java
@@ -31,6 +31,7 @@ public class JpaApplication {
         // 조회
         // 기존) member를 찾고 member의 teamid를 찾아서 team을 찾았음.
         // 이후) member를 찾고 member의 team을 찾음
+        System.out.println("---------------------------");
         Member findMember = memberRepository.findById(member.getId()).get();
         Team findTeam = findMember.getTeam();
         System.out.println("findTeam = " + findTeam.getName());
