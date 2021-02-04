61dbec726e0a699fc3231f5e2c9fadf182288f6f
Jinho <adg0609@ubivelox.com>
2021-02-04 16:26:54 +0900

    일대다-양방향

--- src/main/java/com/spring/jpa/Member.java
+++ src/main/java/com/spring/jpa/Member.java
@@ -17,4 +17,14 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
+    // 일대다 양항뱡(추가)
+    @ManyToOne
+    /*
+    * insertable = false, updatable = false
+    * 위 2개의 옵션을 false로 지정하면 읽기 전용으로 된다.
+    * 참조 Entity를 읽기 전용으로 매핑
+    * */
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
 }
