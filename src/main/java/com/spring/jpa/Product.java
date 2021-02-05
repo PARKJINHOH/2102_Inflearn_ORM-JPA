7fb3d9053b4f3f3ccd4f45ff907da771b3696d92
Jinho <adg0609@ubivelox.com>
2021-02-05 14:54:42 +0900

    다대다 양방향

--- src/main/java/com/spring/jpa/Product.java
+++ src/main/java/com/spring/jpa/Product.java
@@ -6,6 +6,9 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Setter
@@ -17,6 +20,9 @@ public class Product {
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Member> members = new ArrayList<>();
 
 
 }
