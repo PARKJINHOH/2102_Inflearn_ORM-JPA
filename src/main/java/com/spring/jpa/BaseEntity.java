ad0de0ad1663e925d08097f6e5bd465a20a67616
Jino <adg0609>
2021-02-09 18:00:45 +0900

    추상클래스

--- src/main/java/com/spring/jpa/BaseEntity.java
+++ src/main/java/com/spring/jpa/BaseEntity.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Setter
-public class BaseEntity {
+public abstract class BaseEntity {
 
     //  Member와 Team에 공통으로 들어간다.
 //    @Column(name = "INSERT_MEMBER") // 컬럼명 변경도 가능
