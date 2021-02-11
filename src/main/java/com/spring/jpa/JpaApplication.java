0cd23b03b559394793943dce92cc14cea8cee995
Jino <adg0609>
2021-02-11 15:49:38 +0900

    지연로딩

--- src/main/java/com/spring/jpa/JpaApplication.java
+++ src/main/java/com/spring/jpa/JpaApplication.java
@@ -3,6 +3,7 @@ package com.spring.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JpaApplication {
 
@@ -17,8 +18,13 @@ public class JpaApplication {
         this.memberRepository = memberRepository;
         this.teamRepository = teamRepository;
 
+        Team team = new Team();
+        team.setName("team1");
+        teamRepository.save(team);
+
         Member member = new Member();
         member.setName("user1");
+        member.setTeam(team);
         memberRepository.save(member);
 
         // Member를 조회하면 select에 Team까지 Join문 쿼리가 출력된다.
@@ -34,6 +40,14 @@ public class JpaApplication {
         System.out.println(findMember.getId());
         System.out.println(findMember.getName());
 
+        // 그러면 필요할 때만 가지고 오고 싶어할 때 프록시를 생각하면 된다.
+        System.out.println("findMember.class = " + findMember.getTeam().getClass());
+        System.out.println("=========================================");
+
+        // Error가 나지만 실제 Service단에서 실행하면 가능.
+//        System.out.println("getName : " + findMember.getTeam().getName());
+
 
     }
+
 }
