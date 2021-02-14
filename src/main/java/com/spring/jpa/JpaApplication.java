bad1c92cf262acfb15dcc2fdf3e2e22b24da98df
Jino <adg0609>
2021-02-14 22:55:21 +0900

    JPQL INIT

--- src/main/java/com/spring/jpa/JpaApplication.java
+++ src/main/java/com/spring/jpa/JpaApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.persistence.EntityManager;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
-import java.util.List;
 
 
 @SpringBootApplication
@@ -23,48 +19,5 @@ public class JpaApplication {
     private MemberRepository memberRepository;
     private EntityManager em;
 
-    public JpaApplication(MemberRepository memberRepository, EntityManager em) {
-        this.memberRepository = memberRepository;
-        this.em = em;
 
-        //JPQL
-        // @Query("select m from Member m where m.username like '%kim%'")
-        List<Member> byMemberName = memberRepository.findByMemberName();
-
-        System.out.println("===============================");
-
-        // Criteria
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Member> query = cb.createQuery(Member.class);
-
-        // 쿼리를 코드로 작성
-        Root<Member> m = query.from(Member.class);
-        CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
-        List<Member> resultList = em.createQuery(cq).getResultList();
-
-        System.out.println("===============================");
-
-        // QueryDSL
-        // 세팅이 조금 필요하다.
-
-
-
-        // 네이티브 SQL
-        List resultList1 = em.createNativeQuery("select MEMBER_ID, city from MEMBER").getResultList();
-
-        // JDBC 주의사항
-        Member member = new member();
-        member.setUsername("member");
-        em.persist(member);
-
-        em.flush();
-        // em.flush()를 하지 않으면 db에 값이 없기 때문에 결과는 0
-
-        // dbconn.excuteQuery("select * from member");
-        for(Member member1 : resultList){
-            System.out.println("member1 = " + member1);
-        }
-        tx.commit;
-
-    }
 }
