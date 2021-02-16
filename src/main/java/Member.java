fc79a972fbfee9a5247e3d0e457cd75cfee4e973
Jinho <adg0609@naver.com>
2021-02-16 09:56:17 +0900

    init

---
+++ src/main/java/Member.java
@@ -0,0 +1,11 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
