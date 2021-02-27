373205b0e966fce6552df48cfa443f44ca9eea24
Jino <adg0609@naver.com>
2021-02-27 11:40:22 +0900

    페치조인

--- src/main/java/com/jpa/JpaMain.java
+++ src/main/java/com/jpa/JpaMain.java
@@ -17,7 +17,7 @@ public class JpaMain {
         try {
             tx.begin();
 
-            logic1(em);
+            logic2(em);
 
             tx.commit();
         } catch (Exception e) {
@@ -29,39 +29,92 @@ public class JpaMain {
     }
 
     private static void logic1(EntityManager em) {
-        System.out.println("==========경로 표현식 특징===========");
+        System.out.println("==========페치조인 (적용 전)===========");
+
+        Team teamA = new Team();
+        teamA.setName("팀A");
+        em.persist(teamA);
+
+        Team teamB = new Team();
+        teamB.setName("팀B");
+        em.persist(teamB);
 
         Member member1 = new Member();
-        member1.setUsername("관리자1");
+        member1.setUsername("회원1");
+        member1.setTeam(teamA);
         em.persist(member1);
 
         Member member2 = new Member();
-        member2.setUsername("관리자1");
+        member2.setUsername("회원2");
+        member2.setTeam(teamA);
         em.persist(member2);
+
+        Member member3 = new Member();
+        member3.setUsername("회원3");
+        member3.setTeam(teamB);
+        em.persist(member3);
 
         em.flush();
         em.clear();
 
-        // 상태필드 username에서 더이상 참조할 수 있는게 없다.
-        String query1 = "select m.username from Member m";
-//        List<String> result = em.createQuery(query1, String.class).getResultList();
+        String query = "select m from Member m";
+        List<Member> resultList = em.createQuery(query, Member.class).getResultList();
 
-        // 묵시적인 내부 조인 발생 ** 중요, 탐색이 가능하다. m.team.name
-        String query2 = "select m.team from Member m";
-//        List<Team> teamResult = em.createQuery(query2, Team.class).getResultList();
+        for (Member member : resultList) {
+            System.out.println("member = " + member.getUsername() + ", " +
+                    member.getTeam().getName());
+            // 회원1, 팀A(SQL문)
+            // 회원2, 팀A(1차 캐시, 영속성 컨텍스트)
+            // 회원3, 팀A(SQL문)
 
-        // 묵시적 내부 조인 발생, 탐색이 안된다.
-        String query3 = "select t.members from Team t";
-        // From절에서 명시적으로 조인을 통해 탐색이 가능하다.
-        String query31 = "select m.team.name from Team t join t.members m";
-        List result = em.createQuery(query31, Collection.class).getResultList();
+            // 쿼리가 총 3번이 나간다.
+        }
 
-        /*
-        [결론]
-         * 가급적 경로 표현식은 사용하지 않는게 좋다.
-         * 묵시적 조인이 일어나기 때문이다.
-         * 명시적(join 키워드를 직접 입력) 조인을 해야 튜닝하기도 쉽다.
-         */
+    }
+
+    private static void logic2(EntityManager em) {
+        System.out.println("==========페치조인 (적용 후)===========");
+
+        Team teamA = new Team();
+        teamA.setName("팀A");
+        em.persist(teamA);
+
+        Team teamB = new Team();
+        teamB.setName("팀B");
+        em.persist(teamB);
+
+        Member member1 = new Member();
+        member1.setUsername("회원1");
+        member1.setTeam(teamA);
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setUsername("회원2");
+        member2.setTeam(teamA);
+        em.persist(member2);
+
+        Member member3 = new Member();
+        member3.setUsername("회원3");
+        member3.setTeam(teamB);
+        em.persist(member3);
+
+        em.flush();
+        em.clear();
+
+        String query = "select m from Member m join fetch m.team";
+
+        // 프록시가 아니다, 실제 Entity(실제 Data)
+        List<Member> resultList = em.createQuery(query, Member.class).getResultList();
+
+        for (Member member : resultList) {
+            System.out.println("member = " + member.getUsername() + ", " +
+                    member.getTeam().getName());
+            // 회원1, 팀A(SQL문)
+            // 회원2, 팀A(1차 캐시, 영속성 컨텍스트)
+            // 회원3, 팀A(SQL문)
+
+            // 쿼리가 총 3번이 나간다.
+        }
 
     }
 }
